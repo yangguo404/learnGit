d8138608bcbf426cc0f973b5a5f903b8094277ed
yifuqiu <msicr400@163.com>
2020-04-21 22:34:51 +0800

    新增两个文件

---
+++ learnGit/learnGit/Tech.h
@@ -0,0 +1,17 @@
+//
+//  Tech.h
+//  learnGit
+//
+//  Created by yfq on 2020/4/21.
+//  Copyright © 2020 hello. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Tech : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ learnGit/learnGit/Tech.m
@@ -0,0 +1,13 @@
+//
+//  Tech.m
+//  learnGit
+//
+//  Created by yfq on 2020/4/21.
+//  Copyright © 2020 hello. All rights reserved.
+//
+
+#import "Tech.h"
+
+@implementation Tech
+
+@end
