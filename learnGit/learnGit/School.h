23c1ba1e979d914a866e29bffd272944a6f9f89f
yifuqiu <msicr400@163.com>
2020-04-21 22:28:14 +0800

    暂存提交1

---
+++ learnGit/learnGit/School.h
@@ -0,0 +1,17 @@
+//
+//  School.h
+//  learnGit
+//
+//  Created by yfq on 2020/4/21.
+//  Copyright © 2020 hello. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface School : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ learnGit/learnGit/School.m
@@ -0,0 +1,13 @@
+//
+//  School.m
+//  learnGit
+//
+//  Created by yfq on 2020/4/21.
+//  Copyright © 2020 hello. All rights reserved.
+//
+
+#import "School.h"
+
+@implementation School
+
+@end
