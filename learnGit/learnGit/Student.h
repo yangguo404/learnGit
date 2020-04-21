2cfbb96969245ecd2d96c7116ae8e8a8b09f1684
yifuqiu <msicr400@163.com>
2020-04-21 22:33:56 +0800

    暂存功能测试3

---
+++ learnGit/learnGit/Student.h
@@ -0,0 +1,17 @@
+//
+//  Student.h
+//  learnGit
+//
+//  Created by yfq on 2020/4/21.
+//  Copyright © 2020 hello. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Student : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ learnGit/learnGit/Student.m
@@ -0,0 +1,15 @@
+//
+//  Student.m
+//  learnGit
+//
+//  Created by yfq on 2020/4/21.
+//  Copyright © 2020 hello. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+-(void)func1{
+    
+}
+@end
